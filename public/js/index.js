13a861c8eed264bb2eb76307a50c4074994ece84
rlundby <rickardlundby@gmail.com>
2018-03-13 11:25:55 +0100

    jesus

--- public/js/index.js
+++ public/js/index.js
@@ -1,85 +1,63 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDRKZSF6TxNx5d4pFOa_DExL2TgA4UOnu4",
-    authDomain: "we-chat-43a4a.firebaseapp.com",
-    databaseURL: "https://we-chat-43a4a.firebaseio.com",
-    projectId: "we-chat-43a4a",
-    storageBucket: "we-chat-43a4a.appspot.com",
-    messagingSenderId: "357283703015"
-};
-firebase.initializeApp(config);
+let userName;
+let userID;
 
-// Vars for Firebase
-var db = firebase.database();
-var auth = firebase.auth();
-var provider = new firebase.auth.GithubAuthProvider();
-var userId;
-
-
+// Hides the chat by default
+$('#chatApp').hide();
 
 // This is used to log in and verify details
 function toggleSignIn() {
+    $('#loginError').text("");
     if (firebase.auth().currentUser) {
-
         firebase.auth().signOut();
-
     } else {
-        var email = document.getElementById('sign-in-email').value;
-        var password = document.getElementById('sign-in-password').value;
+        let email = document.getElementById('sign-in-email').value;
+        let password = document.getElementById('sign-in-password').value;
         if (email.length < 4) {
-            alert('Please enter an email address.');
+            $('#loginError').text('Please enter an valid email address.');
             return;
         }
         if (password.length < 4) {
-            alert('Please enter a password.');
+            $('#loginError').text('Please enter an valid password.');
             return;
         }
         // Sign in with email and pass.
-        // [START authwithemail]
         firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
             // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
+            let errorCode = error.code;
+            let errorMessage = error.message;
             // [START_EXCLUDE]
             if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password.');
+                $('#loginError').text('Wrong password.');
             } else {
-                alert(errorMessage);
+                console.log(errorMessage);
             }
             console.log(error);
-            document.getElementById('sign-in').disabled = false;
-            // [END_EXCLUDE]
         });
-        // [END authwithemail]
     }
-    document.getElementById('sign-in').disabled = true;
 }
-/**
- * Handles the sign up button press.
- */
+
+// Allows user to create an account
+
 function handleSignUp() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var username = document.getElementById('username').value;
-    var name = document.getElementById('name').value;
-    if (email.length < 4) {
-        alert('Please enter an email address.');
-        return;
-    }
-    if (password.length < 4) {
-        alert('Please enter a password.');
+    $('#regerror').text("");
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let username = document.getElementById('username').value;
+    let name = document.getElementById('name').value;
+    if (password.length < 6) {
+        $('#regerror').text('Please enter a valid password.');
         return;
     }
 
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
         // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        let errorCode = error.code;
+        let errorMessage = error.message;
         // [START_EXCLUDE]
         if (errorCode == 'auth/weak-password') {
-            alert('The password is too weak.');
+            $('#regerror').text('The password is too weak.');
         } else {
-            alert(errorMessage);
+            console.log(errorMessage);
         }
         console.log(error);
         // [END_EXCLUDE]
@@ -98,40 +76,69 @@ function handleSignUp() {
                     name: name,
                 });
             }
-        });
+            userReference.once("value")
+                .then(function(snapshot) {
+                userName = snapshot.child("name").val();
+                $("#startHeader").hide();
+                $("#signinpls").text("Welcome to WeChat!")
+                $(".start-screen").animate({
+                    opacity: 0,
+                }, 1000, "linear", function() {
+                    $(".start-screen").hide();
+                });
+                //load username into header
+                document.getElementById("headerUserName").innerHTML = userName;
+            });
+         });
+            $('#chatApp').show();
+        } else {
+            $('#chatApp').hide();
+            $(".start-screen").show().css("opacity", "1");
+            $(".reg-main").show();
+            $("#signinpls").show().text("Welcome! Please sign in or create an account.");
+            $("#sign-out").hide();
+
         }
     });
 }
 
-function initApp() {
-
-    document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
-    document.getElementById('sign-up').addEventListener('click', handleSignUp, false);
-    document.getElementById('sign-out').addEventListener('click', function(e) {
-        firebase.auth().signOut();
-    });
-}
-
+//  Catch username and edit site
 firebase.auth().onAuthStateChanged( user => {
     if (user) {
-        let username;
+        // Clear errors
+        userID = user.uid;
+        $('#regerror').text("");
+        $('#loginError').text("");
+        // Then
         const userReference = db.ref(`/users/${user.uid}`);
         userReference.once("value")
             .then(function(snapshot) {
-                 username = snapshot.child("name").val();
-                $("#signinpls").text("You are signed in");
-                $("#sign-out").show();
-            })
+                 userName = snapshot.child("name").val();
+                $("#startHeader").hide();
+                $("#signinpls").text("Welcome to WeChat!")
+                $(".start-screen").animate({
+                    opacity: 0,
+                }, 1000, "linear", function() {
+                    $(".start-screen").hide();
+                });
+                //load username into header
+        document.getElementById("headerUserName").innerHTML = userName;
+            });
 
-        $(".reg-main").hide();
+        $('#chatApp').show();
+
     }
      else {
+        $('#chatApp').hide();
+        $(".start-screen").show().css("opacity", "1");
         $(".reg-main").show();
-        $("#signinpls").text("Welcome! Please sign in or create an account.");
+        $("#signinpls").show().text("Welcome! Please sign in or create an account.");
         $("#sign-out").hide();
+
     }
 });
 
-window.onload = function() {
-    initApp();
-};
+document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
+document.getElementById('sign-up').addEventListener('click', handleSignUp, false);
+//document.getElementById('sign-out').addEventListener('click', function(e) {
+  //  firebase.auth().signOut(); });
